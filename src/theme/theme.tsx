79f3d5782216068c64a7b1c107bb71d11c2dd681
kate-lab <katewestbrook@live.co.uk>
2022-11-23 14:56:54 +0000

    theme color palette

---
+++ src/theme/theme.tsx
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    blue: {
+      50: '#aff8ff',
+      900: '#454d66',
+    },
+    purple: {
+      50: '#e6c7ff',
+    },
+    green: {
+      50: '#beff99',
+    },
+    yellow: {
+      50: '#fffda5',
+    },
+  },
+});
